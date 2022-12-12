d2e07ce3c91bc2f30a56deee103f0f48607fb811
sbwml <984419930@qq.com>
2022-12-12 13:52:35 +0800

    Revert "xfs: enable bigtime"
    This reverts commit 4ab7a14eeddd3a2d4fc1d352f8499567d76501e2.

--- luci-app-diskman/luasrc/model/diskman.lua
+++ luci-app-diskman/luasrc/model/diskman.lua
@@ -465,7 +465,7 @@ d.get_format_cmd = function()
     hfsplus = { cmd = "mkfs.hfs", option = "" },
     ntfs = { cmd = "mkntfs", option = "-f" },
     swap = { cmd = "mkswap", option = "" },
-    xfs = { cmd = "mkfs.xfs", option = "-f -m bigtime=1" }
+    xfs = { cmd = "mkfs.xfs", option = "-f" }
   }
   result = {}
   for fmt, obj in pairs(AVAILABLE_FMTS) do
