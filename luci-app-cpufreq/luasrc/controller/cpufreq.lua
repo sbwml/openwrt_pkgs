be3ecb0ec555020f779d5d82a0ed0895a40b7889
Bard <admin@cooluc.com>
2022-12-22 07:25:50 +0800

    fix

--- luci-app-cpufreq/luasrc/controller/cpufreq.lua
+++ luci-app-cpufreq/luasrc/controller/cpufreq.lua
@@ -18,7 +18,7 @@ end
 
 function irq_status()
 	local log_data={}
-	log_data.syslog=luci.sys.exec("cat /proc/interrupts |egrep '^[ ][ ]*(CPU|[0-9]*:)'")
+	log_data.syslog=luci.sys.exec("cat /proc/interrupts | egrep '^[ ]*(CPU|[0-9]*:)'")
 	luci.http.prepare_content("application/json")
 	luci.http.write_json(log_data)
 end
