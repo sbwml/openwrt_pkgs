ce3b2a94a1ee426a2edddc8f5fa669004f02961f
Bard <admin@cooluc.com>
2023-07-31 05:33:37 +0800

    Update release_ram.lua

--- luci-app-ramfree/luasrc/controller/release_ram.lua
+++ luci-app-ramfree/luasrc/controller/release_ram.lua
@@ -1,11 +1,10 @@
 module("luci.controller.release_ram",package.seeall)
 
 function index()
-
 	entry({"admin", "status", "release_ram"}, call("release_ram"), _("Freeping Memory Cache"), 9999)
 end
 
 function release_ram()
-	luci.sys.call("sync && echo 3 > /proc/sys/vm/drop_caches")
+	luci.sys.call("sync && sysctl -w vm.drop_caches=3")
 	luci.http.redirect(luci.dispatcher.build_url("admin/status"))
 end
